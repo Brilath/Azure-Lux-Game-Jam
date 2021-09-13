ea9a079e3833fdfcbc5463df5947b168d9510ea4
Brilath <Brilath@yahoo.com>
2021-09-13 10:00:45 -0400

    Add Story Scene

--- AzureJam2021/Assets/_Project/Code/Controllers/StoryController.cs
+++ AzureJam2021/Assets/_Project/Code/Controllers/StoryController.cs
@@ -4,8 +4,65 @@ using UnityEngine;
 
 public class StoryController : MonoBehaviour
 {
+    [SerializeField] private int _index;
+    [SerializeField] private GameObject _sheepStory;
+    [SerializeField] private AudioClip _sheepStoryClip;
+    [SerializeField] private GameObject _wolfStory;
+    [SerializeField] private AudioClip _wolfStoryClip;
+    [SerializeField] private GameObject _rhymeStory;
+    [SerializeField] private AudioClip _rhymeStoryClip;
+
+    private AudioSource _storyAudioSource;
+
+    private void Awake()
+    {
+        _storyAudioSource = GetComponent<AudioSource>();
+    }
+
     private void Start()
     {
-        SceneController.LoadScene("Game");
+        StartStory(_index);
+    }
+
+    private void Update()
+    {
+        if (_storyAudioSource.isPlaying) return;
+        _index++;
+        StartStory(_index);
+    }
+
+    private void StartStory(int index)
+    {
+        if(index == 0)
+        {
+            EnableStoryArt(_sheepStory);
+            EnableStoryAudio(_sheepStoryClip);
+        }
+        else if (index == 1)
+        {
+            EnableStoryArt(_wolfStory);
+            EnableStoryAudio(_wolfStoryClip);
+        }
+        else if(index == 2)
+        {
+            EnableStoryArt(_rhymeStory);
+            EnableStoryAudio(_rhymeStoryClip);
+        }
+        else
+        {
+            SceneController.LoadScene("Game");
+        }
+    }
+
+    private void EnableStoryArt(GameObject storyArt)
+    {
+        _sheepStory.SetActive(false);
+        _wolfStory.SetActive(false);
+        _rhymeStory.SetActive(false);
+        storyArt.SetActive(true);
+    }
+    private void EnableStoryAudio(AudioClip storyClip)
+    {
+        _storyAudioSource.PlayOneShot(storyClip);
     }
 }
